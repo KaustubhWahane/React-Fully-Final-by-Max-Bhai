ebffb197917ab92abcf3be3c9c3f64478748f7ee
Lavda <kaustubhwahane55@gmail.com>
2024-01-08 16:45:21 +0530

    Section 45 Done

---
+++ react-essentials-start/src/App.jsx
@@ -0,0 +1,46 @@
+import {CORE_CONCEPTS} from './data'
+
+function CoreConcepts(props){
+    return(
+      <li>
+        <img src= {props.image} alt={props.title} />  
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </li>
+    )
+  }
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+
+     <main>
+       <seciton id="core-concepts">
+        {/* Long Method */}
+         <CoreConcepts
+         title={CORE_CONCEPTS[0].title}
+         description={CORE_CONCEPTS[0].description}
+         image = {CORE_CONCEPTS[0].image}
+         />
+         {/* Short-Cut Method */}
+         <CoreConcepts {...CORE_CONCEPTS[1]}/>
+         <CoreConcepts {...CORE_CONCEPTS[2]}/>
+         <CoreConcepts {...CORE_CONCEPTS[3]}/>
+       </seciton>
+     </main>
+    </div>
+  );
+}
+
+export default App;
